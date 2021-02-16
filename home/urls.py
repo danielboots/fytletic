76af5fb19bb7a2c108902c0d6fcc2e5162db6774
Dan Boots <thestereojackers@gmail.com>
2021-02-16 17:17:45 +0000

    tested new found knowledge to link up static pages with out apps, for example: about, contact and a test page, these all work

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("dan", views.dan, name="dan"),
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,27 @@ def index(request):
     """ A view to return the index page. """
 
     return render(request, "home/index.html")
+
+
+def about(request):
+    """
+    A view to return the about page
+    """
+
+    return render(request, "home/about.html")
+
+
+def contact(request):
+    """
+    A view to return the contact page
+    """
+
+    return render(request, "home/contact.html")
+
+
+def dan(request):
+    """
+    A view to return the Dan page
+    """
+
+    return render(request, "home/dan.html")
