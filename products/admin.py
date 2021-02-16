cee137bce7572783c29a3b06ccb7bec2732e0615
Dan Boots <thestereojackers@gmail.com>
2021-02-16 12:10:58 +0000

    customised admin panel

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+# Allows us more power on the backend admin panel
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "sku",
+        "name",
+        "category",
+        "price",
+        "rating",
+        "image",
+    )
+
+    ordering = ("sku",)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "friendly_name",
+        "name",
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
