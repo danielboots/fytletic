6fe23c8b60c77243ad894c3a8baa8c51ce9674ca
Dan Boots <thestereojackers@gmail.com>
2021-02-24 16:36:21 +0000

    signals working checkout success and orders registering in admin dash correctly

--- checkout/signals.py
+++ checkout/signals.py
@@ -9,12 +9,14 @@ def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem update/create
     """
+    print("order recieved signal received")
     instance.order.update_total()
 
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
+    print("delete signal received")
     instance.order.update_total()
