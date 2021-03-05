1d2cbf1db3067165983bc8c022aebcbc4cef55d1
Dan Boots <thestereojackers@gmail.com>
2021-03-05 12:37:56 +0000

    removed secret key and set debug -- deployment to heroku

--- fytletic/settings.py
+++ fytletic/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "wic90tw54s^86!d@y17hx_*)e=#ogsu%xp%#_0_mo2j@jj*9j*"
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ["fytletic.herokuapp.com", "localhost"]
 
