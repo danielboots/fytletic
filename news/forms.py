e993c489c027dff45692368f27f90eaa4c3f635d
Dan Boots <thestereojackers@gmail.com>
2021-03-12 10:07:44 +0000

    testing membership addition field to admin and models.py for products

--- news/forms.py
+++ news/forms.py
@@ -1,8 +1,13 @@
-from .models import Comment
 from django import forms
+
+from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ("name", "body")
+        fields = [
+            "name",
+            "body",
+            "date_added",
+        ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,5 @@
+from django.shortcuts import render
 from .models import Post
-from .forms import CommentForm
-from django.shortcuts import render, get_object_or_404
 
 
 def news(request):
@@ -11,31 +10,6 @@ def news(request):
 
 
 def news_article(request, slug):
-    template_name = "news/news_article.html"
-    post = get_object_or_404(Post, slug=slug)
-    comments = post.comments.filter(active=True)
-    new_comment = None
-    # Comment posted
-    if request.method == "POST":
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
+    post = Post.objects.get(slug=slug)
 
-            # Create Comment object but don't save to database yet
-            new_comment = comment_form.save(commit=False)
-            # Assign the current post to the comment
-            new_comment.post = post
-            # Save the comment to the database
-            new_comment.save()
-    else:
-        comment_form = CommentForm()
-
-    return render(
-        request,
-        template_name,
-        {
-            "post": post,
-            "comments": comments,
-            "new_comment": new_comment,
-            "comment_form": comment_form,
-        },
-    )
+    return render(request, "news/news_article.html", {"post": post})
