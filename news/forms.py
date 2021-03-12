e4acd8aadbdfdc8c880a5b38f37fb1fc9832c42b
Dan Boots <thestereojackers@gmail.com>
2021-03-12 08:43:32 +0000

    running production test to AWS to overide database images

--- news/forms.py
+++ news/forms.py
@@ -1,13 +1,8 @@
-from django import forms
-
 from .models import Comment
+from django import forms
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = [
-            "name",
-            "body",
-            "date_added",
-        ]
+        fields = ("name", "body")
