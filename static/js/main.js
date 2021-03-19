fdd142690bd1384addb9cb73efe97cce66503908
Dan Boots <thestereojackers@gmail.com>
2021-03-19 15:35:38 +0000

    added initial codee for a sidebar menu nav system

--- static/js/main.js
+++ static/js/main.js
@@ -1,43 +1,11 @@
-console.log('is this loading')
-
-/*===== EXPANDER MENU  =====*/
-const showMenu = (toggleId, navbarId, bodyId) => {
-    const toggle = document.getElementById(toggleId),
-        navbar = document.getElementById(navbarId),
-        bodypadding = document.getElementById(bodyId)
-
-    if (toggle && navbar) {
-        toggle.addEventListener('click', () => {
-            navbar.classList.toggle('expander')
-
-            bodypadding.classList.toggle('body-pd')
-        })
-    }
-}
-showMenu('nav-toggle', 'navbar', 'body-pd')
-
-/*===== LINK ACTIVE  =====*/
-const linkColor = document.querySelectorAll('.nav__link')
-function colorLink() {
-    linkColor.forEach(l => l.classList.remove('active'))
-    this.classList.add('active')
-}
-linkColor.forEach(l => l.addEventListener('click', colorLink))
-
-
-/*===== COLLAPSE MENU  =====*/
-const linkCollapse = document.getElementsByClassName('collapse__link')
-var i
-
-for (i = 0; i < linkCollapse.length; i++) {
-    linkCollapse[i].addEventListener('click', function () {
-        const collapseMenu = this.nextElementSibling
-        collapseMenu.classList.toggle('showCollapse')
-
-        const rotate = collapseMenu.previousElementSibling
-        rotate.classList.toggle('rotate')
-    })
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
 }
 
-// alert('test!')
-
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
