199aaee0034dc555bbe51fd7d52f0760057b6d62
Dan Boots <thestereojackers@gmail.com>
2021-03-21 17:04:52 +0000

    new nav bar introduced - transparent sticky nav from Bootsnipp

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,17 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
+$(document).ready(function () {
+    $(".menu-icon").on("click", function () {
+        $("nav ul").toggleClass("showing");
+    });
+});
 
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
+// Scrolling Effect
 
+$(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('black');
+    }
 
+    else {
+        $('nav').removeClass('black');
+    }
+})
