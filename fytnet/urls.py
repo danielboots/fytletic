0bb75a90e9a2e8fa00d01cfdd468c9e4588b5052
Dan Boots <thestereojackers@gmail.com>
2021-03-22 23:17:10 +0000

    added support for front end management of add delete and edit fighters, edit template not working will require mentor support

--- fytnet/urls.py
+++ fytnet/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.fighter, name="fytnet"),
     path("<int:fighter_id>/", views.fytnet_profile, name="fytnet_profile"),
+    path("add/", views.fighter, name="add_fighter"),
+    path("edit/<int:fighter_id>/", views.edit_fighter, name="edit_fighter"),
+    path("delete/<int:fighter_id>/", views.delete_fighter, name="delete_fighter"),
 ]
--- fytnet/views.py
+++ fytnet/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from .forms import FighterForm
 from .models import Fighter
 
 
@@ -28,3 +31,86 @@ def fytnet_profile(request, fighter_id):
     }
 
     return render(request, "fytnet/fighter_profile.html", context)
+
+
+# Edit Fight Profile
+
+
+@login_required
+def add_fighter(request):
+    """ Add a Fighter to your profile """
+    if not request.user.is_superuser:
+        messages.error(request, "Sorry, only store owners can do that.")
+        return redirect(reverse("home"))
+
+    if request.method == "POST":
+        form = FighterForm(request.POST, request.FILES)
+        if form.is_valid():
+            fighter = form.save()
+            messages.success(request, "Successfully added Fighter Profile!")
+            return redirect(reverse("fighter_detail", args=[fighter.id]))
+        else:
+            messages.error(
+                request,
+                "Failed to add Fighter profile. Please ensure the form is valid.",
+            )
+    else:
+        form = FighterForm()
+
+    template = "fytnet/add_fighter.html"
+    context = {
+        "form": form,
+    }
+
+    return render(request, template, context)
+
+
+# Edit Fighter Profile
+
+
+@login_required
+def edit_fighter(request, fighter_id):
+    """ Edit fight profile """
+    if not request.user.is_superuser:
+        messages.error(request, "Sorry, only store owners can do that.")
+        return redirect(reverse("home"))
+
+    fighter = get_object_or_404(Fighter, pk=fighter_id)
+    if request.method == "POST":
+        form = FighterForm(request.POST, request.FILES, instance=fighter)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Successfully updated Fight Profile!")
+            return redirect(reverse("fighter_detail", args=[fighter.id]))
+        else:
+            messages.error(
+                request,
+                "Failed to update Your Fight profile. Please ensure the form is valid.",
+            )
+    else:
+        form = FighterForm(instance=fighter)
+        messages.info(request, f"You are editing {fighter.name}")
+
+    template = "fytnet/edit_fighter_profile.html"
+    context = {
+        "form": form,
+        "fighter": fighter,
+    }
+
+    return render(request, template, context)
+
+
+# Delete Fighter Profile
+
+
+@login_required
+def delete_fighter(request, fighter_id):
+    """ Delete a Fighter from your profile """
+    if not request.user.is_superuser:
+        messages.error(request, "Sorry, only store owners can do that.")
+        return redirect(reverse("home"))
+
+    fighter = get_object_or_404(Fighter, pk=fighter_id)
+    fighter.delete()
+    messages.success(request, "Fighter deleted!")
+    return redirect(reverse("fighter"))
