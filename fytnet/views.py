c7186c994cf93dd8168e6eecd91f47f076b229bc
Dan Boots <thestereojackers@gmail.com>
2021-03-22 14:52:30 +0000

    pagination added to fighter profile... ammend views.py increase number of fighters once database is populated

--- fytnet/views.py
+++ fytnet/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Fighter
 
 
@@ -6,7 +7,15 @@ def fighter(request):
     """ A view to return the main fytnet page. """
     fighters = Fighter.objects.all()
 
-    return render(request, "fytnet/fytnet.html", {"fighters": fighters})
+    # addition of pagination - Followed Traversy media Django dev to deployment.
+
+    paginator = Paginator(fighters, 1)
+    page = request.GET.get("page")
+    paged_listings = paginator.get_page(page)
+
+    context = {"fighters": paged_listings}
+
+    return render(request, "fytnet/fytnet.html", context)
 
 
 def fytnet_profile(request, fighter_id):
