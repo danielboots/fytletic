71be11738ea184fce97b81a76e4b65e38c5d0166
Dan Boots <thestereojackers@gmail.com>
2021-03-27 12:09:15 +0000

    restored commit before gym cache removal

--- gym/models.py
+++ gym/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django_countries.fields import CountryField
 
 
@@ -20,9 +19,6 @@ class GymType(models.Model):
 
 
 class Gym(models.Model):
-
-    # link to user profile - one user one gym
-    gym = models.ForeignKey(User, on_delete=models.CASCADE)
 
     name = models.CharField(max_length=50, null=False, blank=False)
 
