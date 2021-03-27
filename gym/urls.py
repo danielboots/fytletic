c077bc4d83605e68415d2392d82ee32c84e753f1
Dan Boots <thestereojackers@gmail.com>
2021-03-27 12:42:08 +0000

    fixed error in urls.py for paths

--- gym/urls.py
+++ gym/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", views.gym, name="gym"),
     path("<int:gym_id>/", views.gym_profile, name="gym_profile"),
     path("add/", views.add_gym, name="add_gym"),
-    path("edit/<int:fighter_id>/", views.edit_gym, name="edit_gym"),
-    path("delete/<int:fighter_id>/", views.delete_gym, name="delete_gym"),
+    path("edit/<int:gym_id>/", views.edit_gym, name="edit_gym"),
+    path("delete/<int:gym_id>/", views.delete_gym, name="delete_gym"),
 ]
